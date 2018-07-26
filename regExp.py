b535e0b163087b03759067505853a7a324a5bffa
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-07-26 17:17:20 +0200

    Regular Expressions 1

---
+++ regExp.py
@@ -0,0 +1,13 @@
+# Regular Expressions using Python 3.5.2 
+# [0-9] any digit 0,1,2,....,9 
+# + one or more digits 
+# re.findall() give us a list of the matches 
+
+import re 
+x = 'My 2 favorite numbers are 19 and 42' 
+y = re.findall('[0-9]+',x) 
+print(y) # ['2' , '19' , '42' ]
+
+
+
+
