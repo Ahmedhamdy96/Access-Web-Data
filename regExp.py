f110f563cc098bc8aa6ae8e6a0deb61529280055
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-07-26 17:51:32 +0200

    Regular Exprissions 4 - Non-Greedy Matching

--- regExp.py
+++ regExp.py
@@ -1,13 +1,13 @@
-# Greedy Matching in case of using * and + 
+# Non-Greedy Matching in case of using * and + 
 
 # ^F  = Starting with F 
 # .+  = one or more character 
-# :   = Ending with : 
+# ?:   = Ending with : but don't be greedy  
 
 import re
 x = 'From: Using the : character'
-y = re.findall('^F.+:',x)
-print(y) # ['From: Using the :']     Not ['From:']
+y = re.findall('^F.+?:',x)
+print(y) # ['From:']  Not ['From: Using the :']     
 
 
 
