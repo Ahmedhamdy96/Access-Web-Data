3fd564a18a953cdf89ae7298e0e5e9cfde3ef017
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-07-26 18:19:46 +0200

    Fine-Tuning String Extraction 2  ()

--- email.py
+++ email.py
@@ -1,8 +1,10 @@
 # \S+   At Least One Non-Whitespace character
 
+# Parentheses ()  are not part of the match , but they tell WHERE TO START and STOP what string to extract 
+
 import re
 x = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
-y = re.findall('\S+@\S+',x)
+y = re.findall('^From (\S+@\S+)',x)
 print(y) # ['stephen.marquard@uct.ac.za']
 
 
