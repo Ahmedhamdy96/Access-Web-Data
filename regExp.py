9764d3bc7561e5377610ea206939388e4ca433a3
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-07-26 17:38:55 +0200

    Regular Exprissions 3 - Greedy Matching

--- regExp.py
+++ regExp.py
@@ -1,14 +1,13 @@
-# Regular Expressions using Python 3.5.2 
-# [0-9] any digit 0,1,2,....,9 
-# + one or more digits 
-# re.findall() give us a list of the matches 
+# Greedy Matching in case of using * and + 
 
-import re 
-x = 'My 2 favorite numbers are 19 and 42' 
-y = re.findall('[AEIOU]+',x) 
-print(y) # []
+# ^F  = Starting with F 
+# .+  = one or more character 
+# :   = Ending with : 
 
-
+import re
+x = 'From: Using the : character'
+y = re.findall('^F.+:',x)
+print(y) # ['From: Using the :']     Not ['From:']
 
 
 
