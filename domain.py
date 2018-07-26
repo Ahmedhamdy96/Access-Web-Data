3f70b36da10a12e43eb11f6e68a8042a47c2c158
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-07-26 18:42:57 +0200

    get email domain in another expression

--- domain.py
+++ domain.py
@@ -1,9 +1,12 @@
 import re
 lin = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
-y = re.findall('@([^ ]*)',lin)
-# @    = Look Through the string until you find @
-# [^ ] = Non Blank Character  ^ = Not
-# *    = match many of them 
+
+
+y = re.findall( '^From .*@([^ ]*)'   ,lin)
+# ^From = Starting at the beginning of the line 
+# .*@   = after one space and any number of characters followed by @ 
+# get all non-blank characters 
+
 
 print(y) # uct.ac.za
 
