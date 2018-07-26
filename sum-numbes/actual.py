ccb36bb88d0da7bf907da59f7cd1423b69ada402
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2018-07-26 20:41:27 +0200

    actual Data sum

---
+++ sum-numbes/actual.py
@@ -0,0 +1,20 @@
+# a code to read a file contains words and numbers 
+# extract all numbers .. compute the sum of them  
+import re 
+
+h = open('actual.txt')
+file = h.read()
+
+numbers = []
+newNumbers = [] 
+result = 0 
+
+for line in file :
+	numbers = re.findall('[0-9]+',file)  # items is str
+
+for i in numbers : 
+	newNumbers.append(int(i)) # create a new list of integers 
+
+for i in newNumbers : 
+	result += i 
+print(result)
